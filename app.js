b3037d8c47df936f8e3040a2a3998b2a34b33b2a
Alena Buiko <abuiko.ma@gmail.com>
2021-02-28 18:50:16 -0500

    create reviews array data

---
+++ app.js
@@ -0,0 +1,22 @@
+const reviews = [{
+        id: 1,
+        name: "David Kirk",
+        job: "UX Designer",
+        img: "/img/person1.png",
+        text: "Some text about David Kirk here",
+    },
+    {
+        id: 2,
+        name: "Tomas Lewis",
+        job: "Web Developer",
+        img: "/img/person3.png",
+        text: "Some text about Tomas Lewis here",
+    },
+    {
+        id: 3,
+        name: "Selena Jefferson",
+        job: "UI Designer",
+        img: "/img/person2.png",
+        text: "Some text about Selena Jefferson here",
+    }
+]
